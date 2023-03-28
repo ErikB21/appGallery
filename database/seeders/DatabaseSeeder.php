41eb409e503dd0f710f691c9577afcdce7fe1435
ErikBorgogno <erikborgo.20@gmail.com>
2023-03-28 23:34:37 +0200

    creazione Seeders con implementazione nelle Factory

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Album;
+use App\Models\Photo;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        User::truncate();
+        Album::truncate();
+        Photo::truncate();
+        User::factory(20)->has(
+            Album::factory(10)->has(
+                Photo::factory(20)
+            )
+        )->create();
+
+        // $this->call(UserSeeder::class);
+        // $this->call(AlbumSeeder::class);
+        // $this->call(PhotoSeeder::class);
+    }
+}
