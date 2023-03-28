69ab930264e6e4dc266f0ea89293e7987a9b5597
ErikBorgogno <erikborgo.20@gmail.com>
2023-03-28 23:44:55 +0200

    components

---
+++ resources/views/components/alert-info.blade.php
@@ -0,0 +1,8 @@
+<div class="alert alert-info">{{$message}}</div>
+{{-- qui invece mi faccio aiutare dal costruttore della view(app/view),e passerò solo il componente --}}
+
+{{-- <div class="alert alert-info">{{$slot}}</div>
+se uso questa, dovrò creare nel mio albums.blade lo slot all'interno del tag 
+<x-alert_info>
+    {{session()->get('message')}}
+</x-alert_info> --}}
