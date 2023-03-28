310ca6da80bcdd11aeca308cae987a6cb39274af
ErikBorgogno <erikborgo.20@gmail.com>
2023-03-28 23:33:07 +0200

    creazione Modelli, metodo $fillable e relazioni

---
+++ app/Models/Album.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * App\Models\Album
+ *
+ * @property int $id
+ * @property string $album_name
+ * @property string $album_thumb
+ * @property string|null $description
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Photo> $photos
+ * @property-read int|null $photos_count
+ * @method static \Database\Factories\AlbumFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Album newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Album newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Album query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Album whereAlbumName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Album whereAlbumThumb($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Album whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Album whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Album whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Album whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Album whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Album whereUserId($value)
+ * @property-read mixed $path
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Photo> $photos
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Album onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Album withTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Album withoutTrashed()
+ * @mixin \Eloquent
+ */
+class Album extends Model
+{
+    use HasFactory;
+    use SoftDeletes;//trait
+
+    protected $fillable =[
+        'album_name',
+        'description',
+        'user_id',
+        'album_thumb'
+    ];
+
+    // protected $guarded = ['id']; al posto del metodo fillable
+
+    public function photos():HasMany{
+        return $this->hasMany(Photo::class, 'album_id', 'id');
+    }
+
+    //creo un metodo HELPER per la path delle immagini. Se la path inizia con HTTP
+    //allora non add storage, altrimenti si
+    public function getPathAttribute()
+    {
+        $url = $this->album_thumb;
+        if (!str_starts_with($url, 'http')) {//se la stringa non inizia con HTTP
+            $url = 'storage/' . $url;
+        }
+        return $url;
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
---
+++ app/Models/Photo.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * App\Models\Photo
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property string $img_path
+ * @property int $album_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @method static \Database\Factories\PhotoFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Photo newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Photo newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Photo query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Photo whereAlbumId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Photo whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Photo whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Photo whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Photo whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Photo whereImgPath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Photo whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Photo whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Photo onlyTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Photo withTrashed()
+ * @method static \Illuminate\Database\Eloquent\Builder|Photo withoutTrashed()
+ * @mixin \Eloquent
+ */
+class Photo extends Model
+{
+    use HasFactory;
+    use SoftDeletes;//trait
+
+    public function album(){
+        return $this->belongsTo(Album::class);
+    }
+
+    public function getPathAttribute()
+    {
+        $url = $this->img_path;
+        if (!str_starts_with($url, 'http')) { //se la stringa non inizia con HTTP
+            $url = 'storage/' . $url;
+        }
+        return $url;
+    }
+
+}
