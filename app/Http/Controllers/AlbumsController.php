851a8819b3d262633e94f64f644a3eeed551ab53
ErikBorgogno <erikborgo.20@gmail.com>
2023-03-28 23:56:25 +0200

    AlbumController con all crud(validation, authenticable e policy)

---
+++ app/Http/Controllers/AlbumsController.php
@@ -0,0 +1,266 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\AlbumRequest;
+use App\Models\Album;
+use App\Models\Photo;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Storage;
+
+class AlbumsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $albumsPerPage = config('filesystems.albums_per_page');
+        //query builder
+        $queryBuilder = Album::orderBy('id', 'DESC')->withCount('photos');
+        $queryBuilder->where('user_id', Auth::id());
+        if($request->has('id')){
+            $queryBuilder->where('id', '=', $request->input('id'));
+        }
+        if ($request->has('album_name')) {
+            $queryBuilder->where('album_name', 'LIKE', $request->input('album_name').'%');
+        }
+        $albums = $queryBuilder->paginate($albumsPerPage);
+        return view('albums.albums', ['albums' => $albums]);
+
+        //query grezza
+
+        // $sql = 'select * from albums WHERE 1=1 ';
+        // $where = [];
+        // if ($request->has('id')) {
+        //     $where['id'] = $request->get('id');
+        //     $sql .= ' AND ID=:id';
+        // }
+        // if ($request->has('album_name')) {
+        //     $where['album_name'] = $request->get('album_name');
+        //     $sql .= ' AND album_name=:album_name';
+        // }
+        // $sql .= 'ORDER BY id DESC';
+        // //dd($sql);
+        // $albums = DB::select($sql, $where);
+        // return view('albums.albums', ['albums' => $albums]);
+    }
+
+
+
+
+
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Album $album)
+    {
+        return view('albums.createAlbum', compact('album'));
+    }
+
+
+
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(AlbumRequest $request): RedirectResponse
+    {
+        $this->authorize(Album::class);
+        $album = new Album();
+        $album->album_name = request()->input('album_name');
+        $album->description = request()->input('description');
+        $album->user_id = Auth::id();
+        $album->album_thumb = '/';
+        $res = $album->save();
+        if ($res) {
+            if ($this->processFile($album->id, $request, $album)) {
+                $album->save();
+            }
+        }
+        //$res =  Album::create($data);
+        $name = request()->input('name');
+        $messaggio = $res ? 'Album   ' . $name . ' Created' : 'Album ' . $name. ' was not crerated';
+        session()->flash('message', $messaggio);
+        return redirect()->route('albums.index');
+        
+        //query grezza
+        // $data = $request->only(['album_name', 'description']);
+        // $data['user_id'] = 1;
+        // $data['album_thumb'] = '/';
+        // $query = 'INSERT INTO albums (album_name, description, user_id, album_thumb) values (:album_name, :description, :user_id, :album_thumb)';
+        // $res = DB::insert($query, $data);
+    }
+
+    public function processFile($id, Request $req, $album): bool
+    {
+        if (!$req->hasFile('album_thumb')) {
+            return false;
+        }
+
+        $file = $req->file('album_thumb');
+        if (!$file->isValid()) {
+            return false;
+        }
+
+
+        $filename = $id . '.' . $file->extension();
+        $filename = $file->storeAs(env('IMG_DIR'), $filename);
+        $album->album_thumb = $filename;
+        return true;
+    }
+
+
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Album $album)
+    {
+        if(+$album->user_id === +Auth::id()){
+            return $album;
+        }
+        return abort('401');
+    }
+
+
+
+
+
+
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Album $album)
+    {
+        // if($album->user_id !== Auth::id()){
+        //     abort('401');
+        // guardare AuthServiceProvider.php
+        $this->authorize($album);
+        
+        return view('albums.editalbum', compact('album'));
+        // $sql = 'SELECT * FROM albums WHERE id=:id';
+        // $albumEdit = DB::select($sql, ['id' => $album->id]);
+        // return view('albums.editalbum', ['album' => $albumEdit[0]]);
+        // return view('albums.editalbum')->withAlbum($album);
+    }
+
+
+
+
+
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function update(AlbumRequest $req, $id): RedirectResponse
+    {
+
+        $album = Album::find($id);
+
+        $this->authorize($album);
+        $album->album_name = $req->input('album_name');
+        $album->description = $req->input('description');
+        $album->user_id = Auth::id();
+        $this->processFile($id, $req, $album);
+
+        $res = $album->save();
+        $message = $res ? 'Album ' . $album->album_name . ' modificato' : 'Album' . $album->album_name . ' non modificato.';
+        session()->flash('message', $message);
+        return redirect()->route('albums.index');
+
+        // query grezza
+        // $data = $request->only(['album_name', 'description']);
+        // $data['id'] = $album;
+        // $query = 'UPDATE albums set album_name=:album_name, description=:description where id=:id';
+        // $res = Db::update($query, $data);
+        // $message = 'Album con id= ' . $album;
+        // $message .= $res ? ' Aggiornato' : ' Non aggiornato';
+        // session()->flash('message', $message);
+        // return redirect()->route('albums.index');
+    }
+
+
+
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Album  $album
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Album $album)
+    {
+        $this->authorize($album);
+        $thumbnail = $album->album_thumb;
+        $res = $album->delete();
+        
+        if($res && $thumbnail && Storage::exists($thumbnail)){
+            Storage::delete($thumbnail);
+        }
+
+        return $res;
+
+
+        //usare int al posto di Album
+        // return Album::destroy($album);
+        // return Album::findOrFail($album)->delete();
+
+
+
+        //query builder
+        // $res = DB::table('albums')->delete($album);
+        // $res = Album::where('id', $album)->delete();
+        // return $res;
+
+        //query grezza
+        // $sql = 'DELETE FROM albums WHERE id=:id';
+        // return DB::delete($sql, ['id' => $album]);
+    }
+
+    //funzione delete di prova
+    // public function delete($id)
+    // {
+    //     $sql = 'DELETE FROM albums WHERE id=:id';
+    //     return DB::delete($sql, ['id' => $id]);
+    //     // return redirect()->back();
+    // }
+
+    public function getImages($id)
+    {
+        $imgPerPage = config('filesystems.img_per_page');
+        $album = Album::find($id);
+        $album_id = $album->id;
+        $images = Photo::where('album_id',$album->id)->paginate($imgPerPage);
+        //se prima di paginate uso latest() automaticamente verranno inseriti i record in ordine decrescente
+        return view('images.albumImages', compact('album', 'images'));
+    }
+}
