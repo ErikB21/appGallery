48fa23f154957d8306c0df2df448174ee12020c3
ErikBorgogno <erikborgo.20@gmail.com>
2023-03-28 23:36:36 +0200

    custom login e logout

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,76 @@
+<x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+    <section class="vh-100">
+        <div class="container-fluid h-custom" style="height: 90vh;">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+            <div class="col-md-9 col-lg-6 col-xl-5">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                class="img-fluid" alt="Sample image">
+            </div>
+            <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+                <form class="" method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start">
+                        <p class="lead fw-normal mb-0 me-3">Sign in with</p>
+                        <button type="button" class="btn btn-primary btn-floating mx-1">
+                            <i class="fa-brands fa-facebook"></i>
+                        </button>
+
+                        <button type="button" class="btn btn-primary btn-floating mx-1">
+                            <i class="fa-brands fa-twitter"></i>
+                        </button>
+
+                        <button type="button" class="btn btn-primary btn-floating mx-1">
+                            <i class="fa-brands fa-linkedin"></i>
+                        </button>
+                    </div>
+
+                    <div class="divider d-flex align-items-center my-4">
+                        <p class="text-center fw-bold mx-3 mb-0">Or</p>
+                    </div>
+
+                <!-- Email input -->
+                    <div class="form-outline mb-4">
+                            <x-input-label for="email" class="form-label" :value="__('Email')" />
+                            <x-text-input id="email" class="form-control form-control-lg" type="email" name="email" :value="old('email')" placeholder="Enter a valid email address" required autofocus />
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                <!-- Password input -->
+                    <div class="form-outline mb-3">
+                        <x-input-label class="form-label" for="password" :value="__('Password')" />
+
+                        <x-text-input id="password" class="form-control form-control-lg"
+                                        type="password"
+                                        name="password"
+                                        required autocomplete="current-password" placeholder="Enter password" />
+
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div>
+
+                    <div class="d-flex justify-content-between align-items-center">
+                        <!-- Checkbox -->
+                        <div class="form-check mb-0">
+                            <label for="remember_me" class="form-check-label">
+                                <input id="remember_me" type="checkbox" class="form-check-input me-2" name="remember">
+                                <span class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
+                            </label>
+                        </div>
+                        <div class="d-flex align-items-center justify-content-between mt-4">
+                            @if (Route::has('password.request'))
+                                <a class="me-2 underline nav-link text-sm text-secondary hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
+                                    {{ __('Forgot your password?') }}
+                                </a>
+                            @endif
+
+                            <x-primary-button class="ml-3">
+                                {{ __('Log in') }}
+                            </x-primary-button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+</x-guest-layout>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,94 @@
+<x-guest-layout>
+  <section class="my-5 mx-0 px-0">
+    <div class="container h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-lg-12 col-xl-11">
+          <div class="card text-black" style="border-radius: 25px;">
+            <div class="card-body p-md-5">
+              <div class="row justify-content-center">
+                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                  <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
+
+                  <form class="mx-1 mx-md-4" method="POST" action="{{ route('register') }}">
+                  @csrf
+
+                    <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fa-solid fa-user fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                          <x-input-label for="name" class="form-label" :value="__('Name')" />
+                          <x-text-input id="name" class="form-control" type="text" name="name" :value="old('name')" required autofocus />
+                          <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                      </div>
+                    </div>
+
+                    <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                          <x-input-label class="form-label" for="email" :value="__('Email')" />
+                          <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email')" required />
+                          <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                      </div>
+                    </div>
+
+                    <div class="d-flex flex-row align-items-center mb-4">
+                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                        <div class="form-outline flex-fill mb-0">
+                            <x-input-label for="password" class="form-label" :value="__('Password')" />
+
+                              <x-text-input id="password" class="form-control"
+                                              type="password"
+                                              name="password"
+                                              required autocomplete="new-password" />
+
+                              <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                        </div>
+                    </div>
+
+                    <div class="d-flex flex-row align-items-center mb-4">
+                      <i class="fas fa-key fa-lg me-3 fa-fw"></i>
+                      <div class="form-outline flex-fill mb-0">
+                          <x-input-label for="password_confirmation" class="form-label" :value="__('Confirm Password')" />
+
+                          <x-text-input id="password_confirmation" class="form-control"
+                                          type="password"
+                                          name="password_confirmation" required />
+
+                          <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                      </div>
+                    </div>
+
+                    <div class="form-check d-flex justify-content-center mb-5">
+                      <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3c" />
+                      <label class="form-check-label" for="form2Example3">
+                        I agree all statements in <a href="#!">Terms of service</a>
+                      </label>
+                    </div>
+
+                    <div class="d-flex justify-content-center align-items-center mx-4 mb-3 mb-lg-4">
+                          <a class="underline nav-link text-sm text-secondary hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 me-2" href="{{ route('login') }}">
+                              {{ __('Already registered?') }}
+                          </a>
+
+                          <x-primary-button class="ml-4">
+                              {{ __('Register') }}
+                          </x-primary-button>
+                    </div>
+
+                  </form>
+
+                </div>
+                <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+
+                  <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                    class="img-fluid" alt="Sample image">
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</x-guest-layout>
