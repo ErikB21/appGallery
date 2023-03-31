38e2a318b6f212d94375d8d5854ee1c38d0ca944
ErikBorgogno <erikborgo.20@gmail.com>
2023-03-31 12:11:38 +0200

    change template

--- resources/views/albums/albums.blade.php
+++ resources/views/albums/albums.blade.php
@@ -1,47 +1,81 @@
 @extends('templates.default')
 @section('content')
-    <h1 class="text-center my-5">ALBUMS</h1>
-    @if (session()->has('message'))
+    <h1>ALBUMS</h1>
+
+    @if(session()->has('message'))
         <x-alert-info>{{ session()->get('message') }}</x-alert-info>
     @endif
-    <div class="container">
-        <div class=" d-flex justify-content-between flex-wrap">
-            @foreach($albums as $album)
-                <div id="div{{ $album->id }}" class="card my-2" style="width: 18rem;">
+
+
+    <table class="table table-striped table-dark albums">
+        <thead>
+        <tr class="align-middle">
+            <th>Album name</th>
+            <th>Thumb</th>
+            <th>Author</th>
+            <th>Date</th>
+            <th>&nbsp;</th>
+        </tr>
+        </thead>
+        @foreach($albums as $album)
+            <tr class="align-middle" id="tr-{{$album->id}}">
+                <td>({{$album->id}}) {{$album->album_name}}</td>
+                <td>
                     @if($album->album_thumb)
-                        <img class="card-img-top" width="120" src="{{asset($album->path)}}" alt="{{$album->name}}" title="{{$album->name}}">
+                        <img width="120" src="{{asset($album->path)}}" title="{{$album->album_name}}"
+                             alt="{{$album->album_name}}">
+
                     @endif
-                    <div class="card-body d-flex flex-column">
-                        <h3 class="card-title">{{$album->album_name}}</h3>
-                        <h5 class="card-subtitle muted">Id: <strong>{{ $album->id }}</strong></h5>
-                        <p class="card-text">{{ $album->created_at->diffForHumans() }}</p>
-                        <p style="height: 80px;" class="card-text">{{ $album->description }}</p>
-                        <div class="row div-body">
-                            <div class="col-3">
-                                <a href="{{route('photos.create')}}?album_id={{ $album->id }}" data-bs-toggle="tooltip" data-bs-title="New Image" class="btn btn-primary mt-2"><i class="bi bi-plus-circle"></i></a>
-                            </div>
-                            <div class="col-3">
-                                <a href="{{route('albums.images',$album)}}" data-bs-toggle="tooltip" data-bs-title="View Album's Image" class="btn d-flex justify-content-center btn-success mt-2"><i class="bi bi-eye"></i>({{$album->photos_count}})</a>
-                            </div>
-                            <div class="col-3">
-                                <a href="{{route('albums.edit',$album)}}" data-bs-toggle="tooltip" data-bs-title="Update Album" class="btn btn-warning mt-2"><i class="bi bi-recycle"></i></a>
-                            </div>
-                            <div class="col-3">
-                                <form id="form{{ $album->id }}" class="div-btn" method="POST" action="{{route('albums.destroy', $album)}}">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button id="{{ $album->id }}" href="" data-bs-toggle="tooltip" data-bs-title="Delete Album" class="btn btn-danger mt-2"><i class="bi bi-trash3"></i></button>
-                                </form>
-                            </div>
+                </td>
+                <td>{{$album->user->name}}</td>
+                <td>{{$album->created_at->diffForHumans()}}</td>
+                <td>
+                    <div class="row">
+                        <div class="col-md-3">
+                            <a title="Add new image" data-bs-toggle="tooltip" data-bs-title="New Image" href="{{route('photos.create')}}?album_id={{$album->id}}"
+                               class="btn btn-primary">
+                                <i class="bi bi-plus-circle"></i>
+                            </a>
+                        </div>
+                        <div class="col-md-3">
+                            @if($album->photos_count)
+                                <a title="View images" data-bs-toggle="tooltip" data-bs-title="View Images" href="{{route('albums.images',$album)}}"
+                                   class="btn btn-primary">
+                                    <i
+                                        class="bi bi-zoom-in"></i> ({{$album->photos_count}})</a>
+                            @else
+                                <a data-bs-toggle="tooltip" data-bs-title="No Images" disabled class="btn btn-primary pe-none"> <i
+                                        class="bi bi-zoom-in"></i></a>
+                            @endif
+                        </div>
+                        <div class="col-md-3">
+                            <a data-bs-toggle="tooltip" data-bs-title="Update Album" href="{{route('albums.edit',$album)}}" class="btn btn-primary"> <i
+                                    class="bi bi-pen"></i></a>
+                        </div>
+                        <div class="col-md-3">
+                            <form id="form{{$album->id}}" method="POST" action="{{route('albums.destroy',$album)}}"
+                                  class="form-inline">
+                                @method('DELETE')
+
+                                @csrf
+                                <button data-bs-toggle="tooltip" data-bs-title="Delete Album" class="btn btn-danger" id="{{$album->id}}"><i class="bi bi-trash"></i></button>
+                            </form>
                         </div>
                     </div>
+                </td>
+            </tr>
+        @endforeach
+        <tr>
+            <td colspan="5">
+                <div class="row">
+                    <div class="mt-4 container">
+                        {{ $albums->links('pagination::bootstrap-5') }}
+                    </div>
                 </div>
-            @endforeach
-        </div>
-    </div>
-    <div class="mt-4 container">
-        {{ $albums->links('pagination::bootstrap-5') }}
-    </div>
+            </td>
+        </tr>
+    </table>
+
 @endsection
 @section('footer')
     @parent
@@ -49,27 +83,32 @@
         $('document').ready(function () {
             const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
             const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-            $('div.alert-info').fadeOut(4000)//uso per eliminare dopo 4 secondi l'alert dell'aggiornamento
-            $('form.div-btn').on('click', 'button.btn-danger', function (evt) {
-                evt.preventDefault();
-                let id = evt.target.id;
-                let form = $('#form' + id)
-                var urlAlbum = form.attr('action');
-                // we add another parentNode because of the div
-                var cards = $('#div' + id);
+            $('.alert').fadeOut(5000);
+            $('table').on('click', 'button.btn-danger', function (ele) {
+                ele.preventDefault();
+                console.log(ele.target)
+                const id = ele.target.id ? ele.target.id : ele.target.parentNode.id;
+                alert(id)
+                alert('#tr-' + id)
+                const tr = $('#tr-' + id);
+                console.log(tr)
+                const f = $('#form' + id);
+                const urlAlbum = f.attr('action');
                 $.ajax(
                     urlAlbum,
                     {
                         method: 'DELETE',
                         data: {
-                            _token: '{{ csrf_token()}}'
+                            _token: '{{csrf_token()}}'
                         },
                         complete: function (resp) {
                             console.log(resp);
                             if (resp.responseText == 1) {
                                 //   alert(resp.responseText)
-                                cards.remove();
+                                tr.remove();
                                 // $(li).remove();
+                            } else {
+                                alert('Problem contacting server');
                             }
                         }
                     }
