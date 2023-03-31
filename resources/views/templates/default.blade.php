d5d97748323c275c74048280d7f6f979b19d3a2a
ErikBorgogno <erikborgo.20@gmail.com>
2023-03-31 13:45:42 +0200

    add libreria Lightbox

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -10,6 +10,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link href="/css/lightbox.css" rel="stylesheet" />
     <style>
         body {
             padding: 70px 15px 0;
@@ -90,6 +91,7 @@
 </main><!-- /.container -->
 @section('footer')
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+    <script src="/js/lightbox.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
             crossorigin="anonymous"></script>
