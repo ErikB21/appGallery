087a3dc9d1d2c1d69e9156e21ab326534f588e19
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-01 17:27:38 +0200

    add route for Gallerycontroller(showcategoryAlbums)

--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,7 @@ Route::get('/dashboard', function () {
 Route::group(['prefix' => 'gallery'], function(){
     Route::get('/', [GalleryController::class, 'index'])->name('gallery.index');
     Route::get('album/{album}/images', [GalleryController::class, 'showAlbumImages'])->name('gallery.album.images');
+    Route::get('categories/{category}/albums', [GalleryController::class, 'showCategoryAlbums'])->name('gallery.categories.albums');
 });
 
 Route::resource('categories', CategoryController::class);
