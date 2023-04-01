6f0982334fb9dbcc669fe0038167cbce0bf9bd07
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-01 18:21:06 +0200

    add new Method, change index and showAlbumImages for categories

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -2,17 +2,23 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Album;
-use App\Models\Photo;
+use App\Models\{ Album, Category, Photo };
 use Illuminate\Http\Request;
 
 class GalleryController extends Controller
 {
     public function index(){
-        return view('gallery.albums')->with('albums', Album::latest()->paginate());
+        $albums = Album::with('categories')->latest()->paginate(8);
+        return view('gallery.albums')->with(['albums' => $albums, 'category_id' => null]);
     }
 
     public function showAlbumImages(Album $album){
-        return view('gallery.images',['images' => Photo::whereAlbumId($album->id)->paginate(8), 'album' => $album ]);
+        $photos = Photo::whereAlbumId($album->id)->paginate(8);
+        return view('gallery.images',['images' => $photos, 'album' => $album ]);
+    }
+
+    public function showCategoryAlbums(Category $category){
+        $cat = $category->albums()->with('categories')->latest()->paginate();
+        return view('gallery.albums')->with(['albums' => $cat, 'category_id' => $category->id]);
     }
 }
