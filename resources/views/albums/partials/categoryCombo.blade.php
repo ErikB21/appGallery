cc606960dbd00917f0fbfbe973c9616f135bea41
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-03 11:45:55 +0200

    partials change from select to checkbox

--- resources/views/albums/partials/categoryCombo.blade.php
+++ resources/views/albums/partials/categoryCombo.blade.php
@@ -1,11 +1,19 @@
-<div class="form-group">
+{{-- <div class="form-group">
     <label for="categories" class="form-label">
         <h4>Categories</h4>
     </label>
-    <select  name="categories[]" id="categories" class="form-control" multiple>
+    <select  name="categories[]" id="categories" class="form-control" multiple="true" size="8">
         @foreach($categories as $cat)
-            <option value="{{$cat->id}}">{{Ucwords($cat->category_name)}}</option>
+            <option {{ in_array($cat->id, $selectedCategories, true)? 'selected' : '' }} value="{{$cat->id}}">{{Ucwords($cat->category_name)}}</option>
         @endforeach
     </select>
 
+</div> --}}
+<div class="row">
+    <div class="form-check col-1">
+        @foreach($categories as $cat)
+            <input {{ in_array($cat->id, $selectedCategories, true)? 'checked' : ''  }} name="categories[]" type="checkbox" class="form-check-input" id="categories_{{$cat->id}}" value="{{$cat->id}}">
+            <label class="form-check-label" for="categories">{{ Ucwords($cat->category_name) }}</label>
+        @endforeach
+    </div>
 </div>
