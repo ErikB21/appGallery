34010727e2edca1e08b3511d91628741148f9a4a
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-03 15:30:08 +0200

    add withCount

--- app/Models/User.php
+++ app/Models/User.php
@@ -97,6 +97,6 @@ class User extends Authenticatable
 
     public function categories(): HasMany
     {
-        return $this->hasMany(Category::class);
+        return $this->hasMany(Category::class)->withCount('albums');
     }
 }
