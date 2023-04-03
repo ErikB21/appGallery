7bfdd5bf3867183e7f09a1c28793452e75c7e456
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-04 00:08:30 +0200

    add pagination and change card size

--- resources/views/gallery/albums.blade.php
+++ resources/views/gallery/albums.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             @foreach ($albums as $album)
                 <div class="col-6 col-lg-4 col-xl-3">
-                    <div class="card m-2">
+                    <div class="card m-2" style="height: 500px;">
                         <a href="{{ route('gallery.album.images', $album->id) }}">
                             @if($album->album_thumb)
                                 <img style="width: 100%; height: 170px;"  class="card-img-top img-fluid rounded" src="{{asset($album->path)}}" alt="{{$album->name}}" title="{{$album->name}}">
@@ -16,7 +16,7 @@
                                 <h3 class="card-title">{{$album->album_name}}</h3>
                             </a> 
                             <p class="card-text">{{ $album->created_at->diffForHumans() }}</p>
-                            <p style="height: 90px;" class="card-text">{{ $album->description }}</p>
+                            <p style="height: 110px;" class="card-text">{{ $album->description }}</p>
                             <div class="my-4">
                                 @foreach ($album->categories as $cat )
                                 @if ($cat->id !== $category_id)
@@ -37,7 +37,10 @@
                         </div>
                     </div>
                 </div>
-            @endforeach 
+            @endforeach
         </div>
+    </div>
+    <div class="container mt-3">
+        {{ $albums->links('pagination::bootstrap-5') }}
     </div>
 @endsection
