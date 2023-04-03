d829a53b2ec01e9ca1ad532c9bb236823561321a
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-04 00:35:44 +0200

    new view create category

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,14 @@
+@extends('templates\default')
+
+@section('content')
+
+    <h1 class="text-center mb-5">{{ $category->category_name ? 'Modify category' : 'New category' }}</h1>
+    <div class="container">
+        <div class="row d-flex justify-content-center">
+            <div class="col-auto">
+                @include('categories\categoryForm')
+            </div>
+        </div>
+    </div>
+
+@endsection
