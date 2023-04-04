dd5c49854cf0c0551b0ea61d2875fd4df5f4f1a0
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-04 18:32:44 +0200

    change span in div

--- resources/views/components/alert-info.blade.php
+++ resources/views/components/alert-info.blade.php
@@ -1,4 +1,4 @@
-<span class="alert alert-info">{{$message}}</span>
+<div class="alert alert-info m-o">{{$message}}</div>
 {{-- qui invece mi faccio aiutare dal costruttore della view(app/view),e passerò solo il componente --}}
 
 {{-- <div class="alert alert-info">{{$slot}}</div>
