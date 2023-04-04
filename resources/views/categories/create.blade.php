c737fb805ae15f5c6643a5cd873347684b32f38c
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-04 02:23:14 +0200

    include partials for errors

--- resources/views/categories/create.blade.php
+++ resources/views/categories/create.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 
     <h1 class="text-center mb-5">{{ $category->category_name ? 'Modify category' : 'New category' }}</h1>
+    @include('categories.partials.inputErrors')
     <div class="container">
         <div class="row d-flex justify-content-center">
             <div class="col-auto">
