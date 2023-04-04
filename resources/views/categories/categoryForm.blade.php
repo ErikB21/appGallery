aae3a71e77e0c7e3192a5d86e01b7932fd50f803
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-04 15:26:26 +0200

    change categoryForm with ternario

--- resources/views/categories/categoryForm.blade.php
+++ resources/views/categories/categoryForm.blade.php
@@ -1,9 +1,26 @@
-<form style="width: 80%" action="{{ route('categories.store') }}" method="POST" class="row">
+@if($category->category_name)
+    <h1 class="text-center mb-5">Modify Category</h1>
+    <form action="{{ route('categories.update', $category->id) }}" method="POST" class="row">
+        @method('PATCH')
+@else
+    <h1 class="text-center mb-5">New Category</h1>
+    <form action="{{ route('categories.store') }}" method="POST" class="row">
+@endif
+
     @csrf
     <div class="form-group mt-3">
-        <input type="text" required minlength="3" placeholder="Category" class="form-control" name="category_name" id="category_name">
+        <input type="text" required minlength="3" value="{{ old('category_name', $category->category_name) }}" placeholder="Category" class="form-control" name="category_name" id="category_name">
     </div>
     <div class="form-group mt-3 d-flex justify-content-center">
-        <button class="btn btn-primary">Save</button>
+        @if($category->category_name)
+            <button class="btn btn-warning mt-2 mx-3"><i class="bi bi-pen"></i> Update</button>
+    </form>
+            <form  action="{{ route('categories.destroy', $category->id) }}" method="POST" class="row">
+                @csrf
+                @method('DELETE')
+                <button class="mx-3 btn btn-danger mt-2" id="{{$category->id}}"><i class="bi bi-trash"></i> Delete Category</button>
+            </form>
+        @else
+            <button class="btn btn-primary mt-2"><i class="bi bi-save"></i> Save</button>
+        @endif
     </div>
-</form>
