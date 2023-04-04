fe8fdbc61230a6a162f6f6659dfffb9347726f20
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-04 02:22:48 +0200

    change size, add required and minlenght

--- resources/views/categories/categoryForm.blade.php
+++ resources/views/categories/categoryForm.blade.php
@@ -1,7 +1,7 @@
-<form action="{{ route('categories.store') }}" method="POST" class="row">
+<form style="width: 80%" action="{{ route('categories.store') }}" method="POST" class="row">
     @csrf
     <div class="form-group mt-3">
-        <input type="text" placeholder="Category" class="form-control" name="category_name" id="category_name">
+        <input type="text" required minlength="3" placeholder="Category" class="form-control" name="category_name" id="category_name">
     </div>
     <div class="form-group mt-3 d-flex justify-content-center">
         <button class="btn btn-primary">Save</button>
