0abd63eceb411ae7d0552107d9a976353dffc3f4
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-07 02:03:14 +0200

    change route AdminUsersController

--- routes/admin.php
+++ routes/admin.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Admin\AdminUsersController;
 use Illuminate\Support\Facades\Route;
 
     Route::resource('users', AdminUsersController::class);
-    Route::get('getUsers', [AdminUsersController::class, 'getUsers']);
+    Route::get('getUsers', [AdminUsersController::class, 'getUsers'])->name('admin.getUsers');
     Route::view('/', 'templates/admin')->name('admin');
     
 
