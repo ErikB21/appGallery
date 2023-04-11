20026494da778eec52a88658a3500f6e60ddd2d1
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-11 12:44:55 +0200

    create a new mailNotifyAdmin, with user, album and method content

---
+++ app/Mail/MailNotifyAdminNewAlbum.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Album;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class MailNotifyAdminNewAlbum extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(public User $admin, public Album $album)
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     *
+     * @return \Illuminate\Mail\Mailables\Envelope
+     */
+    public function envelope()
+    {
+        return new Envelope(
+            subject: 'Mail Notify Admin New Album',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     *
+     * @return \Illuminate\Mail\Mailables\Content
+     */
+    public function content()
+    {
+        return new Content(
+            markdown: 'mails.mailnotifyadminnewalbum',
+            with: ['admin' => $this->admin, 'album' => $this->album]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+}
