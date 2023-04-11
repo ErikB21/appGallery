3bbe149590d7704b4b87eb3a24b8c958926b696a
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-11 14:24:21 +0200

    change template mail

--- resources/views/mails/mailnotifyadminnewalbum.blade.php
+++ resources/views/mails/mailnotifyadminnewalbum.blade.php
@@ -1,16 +1,24 @@
-@component('mail::message')
-
-    Hello {{ $admin->name }}
-
-    # New Album {{ $album->album_name }} created!
-
-    Visit
-
-    @component('mail::button', ['url' => route('albums.edit', $album->id)])
-       {{ $album->album_name }}
-    @endcomponent
-
-    Thanks,<br>
-    {{ config('app.name') }}
-@endcomponent
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Email for New Album</title>
+</head>
+<body>
+    <div class="card text-center">
+        <div class="card-header">
+            <h1 class="text-center">Hello {{ $admin->name }}</h1>
+        </div>
+        <div class="card-body">
+            <h5 class="card-title">Nuovo Album</h5>
+            <p class="card-text">L'Album {{ $album->album_name }} è stato creato con successo!</p>
+            <a class="btn btn-dark" href="{{ route('albums.edit', $album->id) }}">{{ $album->album_name }}</a>
+        </div>
+        <div class="card-footer text-muted">
+            Thanks, <br> {{ config('app.name') }}
+        </div>
+    </div>
+</body>
+</html>
