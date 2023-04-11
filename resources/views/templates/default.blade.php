a8adcb02e375502b32a20f55ec1ede9a959250f8
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-11 11:10:44 +0200

    add href for Gallery

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -27,7 +27,7 @@
     <!-- Fixed navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">GALLERY</a>
+            <a class="navbar-brand" href="{{ route('gallery.index') }}">GALLERY</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
                     aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
