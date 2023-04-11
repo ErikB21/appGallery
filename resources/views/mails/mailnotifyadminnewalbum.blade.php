6f512e97685b6eb8e7418539ad1e1baf50eaa38f
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-11 12:45:08 +0200

    new view for mail

---
+++ resources/views/mails/mailnotifyadminnewalbum.blade.php
@@ -0,0 +1,16 @@
+@component('mail::message')
+
+    Hello {{ $admin->name }}
+
+    # New Album {{ $album->album_name }} created!
+
+    Visit
+
+    @component('mail::button', ['url' => route('albums.edit', $album->id)])
+       {{ $album->album_name }}
+    @endcomponent
+
+    Thanks,<br>
+    {{ config('app.name') }}
+@endcomponent
+
