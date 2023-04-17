047be3aebad51c155ec90ab9bd3d1d3e88ffda84
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-17 15:28:00 +0200

    change template

--- resources/views/gallery/albums.blade.php
+++ resources/views/gallery/albums.blade.php
@@ -1,39 +1,117 @@
 @extends('templates\default')
 
+<style>
+    .box {
+        position: relative;
+        width: 300px;
+        height: 300px;
+        margin: 20px;
+        transform-style: preserve-3d;
+        perspective: 1000px;
+        cursor: pointer;
+    }
+
+    .box .body {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        transform-style: preserve-3d;
+        transition: 0.9s ease;
+    }
+
+.box .body .imgContainer {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    transform-style: preserve-3d;
+}
+
+.box .body .imgContainer img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+}
+
+.box .body .content {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 300px;
+    height: 300px;
+    background: #333;
+    backface-visibility: hidden;
+    transform-style: preserve-3d;
+    transform: rotateY(180deg);
+}
+
+.box:hover .body {
+    transform: rotateY(180deg);
+}
+
+.box .body .content div {
+    transform-style: preserve-3d;
+    display: flex;
+    width: 300px;
+    height: 300px;
+    margin: 20px;
+    padding: 10px;
+    flex-direction: column;
+    background-image: radial-gradient(100% 100% at 100% 0, #5adaff 0, #4D759D 100%);
+    transform: translateZ(100px);
+}
+
+
+
+</style>
+
+
+
 @section('content')
     <div class="container">
+        <h1 class="text-center mt-3 mb-5">WorldGallery</h1>
         <div class="row">
             @foreach ($albums as $album)
-                <div class="col-6 col-lg-4 col-xl-3">
-                    <div class="card m-2" style="height: 500px;">
-                        <a href="{{ route('gallery.album.images', $album->id) }}">
-                            @if($album->album_thumb)
-                                <img style="width: 100%; height: 170px;"  class="card-img-top img-fluid rounded" src="{{asset($album->path)}}" alt="{{$album->name}}" title="{{$album->name}}">
-                            @endif
-                        </a>
-                        <div class="card-body">
-                            <a class="nav-link" href="{{ route('gallery.album.images', $album->id) }}">
-                                <h3 class="card-title">{{$album->album_name}}</h3>
-                            </a> 
-                            <p class="card-text">{{ $album->created_at->diffForHumans() }}</p>
-                            <p style="height: 110px;" class="card-text">{{ $album->description }}</p>
-                            <div class="my-4">
-                                @foreach ($album->categories as $cat )
-                                @if ($cat->id !== $category_id)
-                                    <a class="text-decoration-none" href="{{ route('gallery.categories.albums', $cat->id) }}">
-                                        <span class="badge text-bg-secondary">
-                                            {{ $cat->category_name }}
-                                        </span>
-                                    </a>
-                                @else
-                                    <span class="badge text-bg-success">
-                                        {{ $cat->category_name }}
-                                    </span>
-                                @endif
-                                    
-                                @endforeach
+                <div class="col-xs-12 col-sm-6 col-lg-4 px-1 pb-4 m-auto d-flex justify-content-between">
+                    <div class="box">
+                        <div class="body">
+                            <div class="imgContainer">
+                                <a href="{{ route('gallery.album.images', $album->id) }}">
+                                    @if($album->album_thumb)
+                                        <img class="img_flip" src="{{asset($album->path)}}" alt="{{$album->name}}" title="{{$album->name}}">
+                                    @endif
+                                </a>
                             </div>
-                            <span class="card-text">Creato da <strong>{{ $album->user->name }}</strong></span>
+                            <div class="content d-flex flex-column align-items-center justify-content-center">
+                                <div>
+                                    <a style="height: 40px;" class="nav-link" href="{{ route('gallery.album.images', $album->id) }}">
+                                        <h2 class="card-title text-light">{{$album->album_name}}</h2>
+                                    </a>
+                                    <p style="height: 20px;" class="card-text">{{ $album->created_at->diffForHumans() }}</p>
+                                    <p style="height: 180px;" class="card-text">{{ $album->description }}</p>
+                                    <p style="height: 60px;" class="my-2 d-flex justify-content-evenly flex-wrap">
+                                        @foreach ($album->categories as $cat )
+                                            @if ($cat->id !== $category_id)
+                                                <a class="text-decoration-none mx-1" href="{{ route('gallery.categories.albums', $cat->id) }}">
+                                                    <span class="badge text-bg-secondary">
+                                                        {{ $cat->category_name }}
+                                                    </span>
+                                                </a>
+                                            @else
+                                                <span class="badge text-bg-success">
+                                                    {{ $cat->category_name }}
+                                                </span>
+                                            @endif             
+                                        @endforeach
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
