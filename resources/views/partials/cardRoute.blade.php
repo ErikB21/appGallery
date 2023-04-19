11f6a915382a5c4a8a7ea27826a5f2af2bd8b65e
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-19 13:50:14 +0200

    change color card

--- resources/views/partials/cardRoute.blade.php
+++ resources/views/partials/cardRoute.blade.php
@@ -8,7 +8,7 @@
 <div class="container mt-5">
         <div class="row">
             <div class="col-xl-4 col-md-6">
-                <div class="card bg-primary text-white mb-4">
+                <div class="card text-white mb-4">
                     <div class="card-body d-flex justify-content-center">
                         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-images" viewBox="0 0 16 16">
                             <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
@@ -18,12 +18,12 @@
                     <div class="card-footer d-flex flex-column align-items-center justify-content-between">
                         <h3>Visiona i tuoi Album</h3>
                         <p>La tua galleria, con i tuoi album e le tue fotografie, modifica o cancella.</p>
-                        <a class="btn btn-outline-dark" href="{{ route('albums.index') }}"><i class="bi bi-journal-album pe-2"></i> Vedi Album</a>
+                        <a class="btn btn-outline-light" href="{{ route('albums.index') }}"><i class="bi bi-journal-album pe-2"></i> Vedi Album</a>
                     </div>
                 </div>
             </div>
             <div class="col-xl-4 col-md-6">
-                <div class="card bg-warning text-white mb-4">
+                <div class="card text-white mb-4">
                     <div class="card-body d-flex justify-content-center">
                         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-journal-album" viewBox="0 0 16 16">
                             <path d="M5.5 4a.5.5 0 0 0-.5.5v5a.5.5 0 0 0 .5.5h5a.5.5 0 0 0 .5-.5v-5a.5.5 0 0 0-.5-.5h-5zm1 7a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1h-3z"/>
@@ -34,12 +34,12 @@
                     <div class="card-footer d-flex flex-column align-items-center justify-content-between">
                         <h3>Nuovi Album</h3>
                         <p>Crea un nuovo Album, aggiungi una foto e una descrizione.</p>
-                        <a class="btn btn-outline-dark" href="{{ route('albums.create') }}"><i class="bi bi-plus-circle pe-2"></i>Crea Album</a>
+                        <a class="btn btn-outline-light" href="{{ route('albums.create') }}"><i class="bi bi-plus-circle pe-2"></i>Crea Album</a>
                     </div>
                 </div>
             </div>
             <div class="col-xl-4 col-md-6">
-                <div class="card bg-success text-white mb-4">
+                <div class="card text-white mb-4">
                     <div class="card-body d-flex justify-content-center">
                         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-image-fill" viewBox="0 0 16 16">
                             <path d="M.002 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2V3zm1 9v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V9.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12zm5-6.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0z"/>
@@ -48,12 +48,12 @@
                     <div class="card-footer d-flex flex-column align-items-center justify-content-between">
                         <h3>Nuove Immagini</h3>
                         <p>Aggiungi una nuova Immagine, scegli genere e tipo di Album.</p>
-                        <a class="btn btn-outline-dark" href="{{ route('photos.create') }}"><i class="bi bi-image pe-2"></i>Nuova Immagine</a>
+                        <a class="btn btn-outline-light" href="{{ route('photos.create') }}"><i class="bi bi-image pe-2"></i>Nuova Immagine</a>
                     </div>
                 </div>
             </div>
             <div class="col-xl-4 col-md-6">
-                <div class="card bg-danger text-white mb-4">
+                <div class="card text-white mb-4">
                     <div class="card-body d-flex justify-content-center">
                         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-tags-fill" viewBox="0 0 16 16">
                             <path d="M2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2zm3.5 4a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
@@ -63,13 +63,13 @@
                     <div class="card-footer d-flex flex-column align-items-center justify-content-between">
                         <h3>Visiona le Categorie</h3>
                         <p>Vedi le tue categorie, aggiungine di nuove oppure modificale. Scegli tu!</p>
-                        <a class="btn btn-outline-dark" href="{{ route('categories.index') }}"><i class="bi bi-tag pe-2"></i>Vedi Categorie</a>
+                        <a class="btn btn-outline-light" href="{{ route('categories.index') }}"><i class="bi bi-tag pe-2"></i>Vedi Categorie</a>
                     </div>
                 </div>
             </div>
             @if(Auth::user()->user_role === 'admin')
                 <div class="col-xl-4 col-md-6">
-                    <div class="card bg-info text-white mb-4">
+                    <div class="card text-white mb-4">
                         <div class="card-body d-flex justify-content-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-envelope-at-fill" viewBox="0 0 16 16">
                                 <path d="M2 2A2 2 0 0 0 .05 3.555L8 8.414l7.95-4.859A2 2 0 0 0 14 2H2Zm-2 9.8V4.698l5.803 3.546L0 11.801Zm6.761-2.97-6.57 4.026A2 2 0 0 0 2 14h6.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586l-1.239-.757ZM16 9.671V4.697l-5.803 3.546.338.208A4.482 4.482 0 0 1 12.5 8c1.414 0 2.675.652 3.5 1.671Z"/>
@@ -79,12 +79,12 @@
                         <div class="card-footer d-flex flex-column align-items-center justify-content-between">
                             <h3>Email</h3>
                             <p>Ciao {{ Auth::user()->name }}, visiona le tue Email. Visualizza e cancella i tuoi messaggi!</p>
-                            <a class="btn btn-outline-dark" href="https://mailtrap.io/inboxes"><i class="bi bi-envelope pe-2"></i>Email</a>
+                            <a class="btn btn-outline-light" href="https://mailtrap.io/inboxes"><i class="bi bi-envelope pe-2"></i>Email</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-xl-4 col-md-6">
-                    <div class="card bg-dark text-white mb-4">
+                    <div class="card text-white mb-4">
                         <div class="card-body d-flex justify-content-center">
                             <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
                                 <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/>
@@ -100,6 +100,12 @@
             @endif
         </div>
     </div>
+
+    <style>
+        .card{
+            background-color: #0A4067;
+        }
+    </style>
 @section('footer')
     @parent
     <script>
