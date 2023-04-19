292f321ebc17f80081fa1a8038b16e95f5c8ba46
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-19 12:15:19 +0200

    change layout and add js for div.alert-info

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,11 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="my-5">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <h1 class="text-center">Benvenuto {{ Auth::user()->name }}</h1>
-                </div>
-            </div>
-        </div>
-    </div>
     @include('partials\cardRoute')
 </x-app-layout>
+@section('footer')
+    @parent
+    <script>
+        $('document').ready(function () {
+            $('div.alert-info').fadeOut(4000)//uso per eliminare dopo 4 secondi l'alert dell'aggiornamento
+        });
+    </script>
+@endsection
