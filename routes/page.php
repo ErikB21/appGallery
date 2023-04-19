259eb872ee3e488ebff8c6f6b3418729f3a131e5
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-19 12:14:26 +0200

    comment because file's error

--- routes/page.php
+++ routes/page.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\PageController;
-use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\PageController;
+// use Illuminate\Support\Facades\Route;
 
-Route::get('/about', [PageController::class, 'about']);
-Route::get('/blog', [PageController::class, 'blog']);
-Route::get('/staff', [PageController::class, 'staff']);
+// Route::get('/about', [PageController::class, 'about']);
+// Route::get('/blog', [PageController::class, 'blog']);
+// Route::get('/staff', [PageController::class, 'staff']);
