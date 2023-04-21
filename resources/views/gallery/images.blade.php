87568ff3a9ec2997ab881f79b740e5bfef9022ea
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-21 18:30:50 +0200

    remove width

--- resources/views/gallery/images.blade.php
+++ resources/views/gallery/images.blade.php
@@ -5,10 +5,10 @@
         <div class="row">
             @forelse ($images as $image)
                 <div class="col-6 col-lg-4 col-xl-3">
-                    <div class="card m-2" style="width: 18rem;">
-                        <a href="{{asset($image->path)}}" data-lightbox="{{ $album->album_name }}">
+                    <div class="card m-2">
+                        <a href="{{asset($image->img_path)}}" data-lightbox="{{ $album->album_name }}" data-title="{{ $image->name }}" >
                             @if($image->img_path)
-                                <img class="card-img-top img-fluid rounded" src="{{asset($image->path)}}" alt="{{$image->name}}" title="{{$image->name}}">
+                                <img class="card-img-top img-fluid rounded" src="{{asset($image->img_path)}}" alt="{{$image->name}}" title="{{$image->name}}">
                             @endif
                         </a>
                         <div class="card-body">
@@ -29,4 +29,14 @@
             </div>
         </div>
     </div>
+@endsection
+
+@section('footer')
+    @parent
+    <script>
+        lightbox.option({
+        'maxWidth': 200,
+        'maxHeight': 200
+        })
+    </script>
 @endsection
