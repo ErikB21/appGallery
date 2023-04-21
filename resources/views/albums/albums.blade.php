d313d122cc27d04949af44421bbf9a7c51f7c7ff
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-21 18:38:47 +0200

    remove tooltip js

--- resources/views/albums/albums.blade.php
+++ resources/views/albums/albums.blade.php
@@ -22,10 +22,10 @@
         <tbody>
             @foreach($albums as $album)
                 <tr class="align-middle" id="tr-{{$album->id}}">
-                    <td data-bs-toggle="tooltip" data-bs-title="Id: {{$album->id}}">{{$album->album_name}}</td>
+                    <td title="Id: {{$album->id}}">{{$album->album_name}}</td>
                     <td>
                         @if($album->album_thumb)
-                            <img width="100" data-bs-toggle="tooltip" data-bs-title="Creato da {{ $album->user->name }}" src="{{asset($album->path)}}" title="{{$album->album_name}}"
+                            <img width="100" title="Creato da {{ $album->user->name }}" src="{{asset($album->path)}}" title="{{$album->album_name}}"
                                 alt="{{$album->album_name}}">
 
                         @endif
@@ -39,21 +39,21 @@
                     <td>
                         <div class="row">
                             <div class="col-md-3 mx-2 mx-lg-0 col-lg-4">
-                                <a title="Add new image" data-bs-toggle="tooltip" data-bs-title="New Image" href="{{route('photos.create')}}?album_id={{$album->id}}"
+                                <a title="Add new image" title="New Image" href="{{route('photos.create')}}?album_id={{$album->id}}"
                                 class="btn btn-primary">
                                     <i class="bi bi-plus-circle"></i>
                                 </a>
                             </div>
                             <div class="col-md-3 mx-2 mx-lg-0 col-lg-4">
                                 @if($album->photos_count)
-                                    <a title="View images" data-bs-toggle="tooltip" data-bs-title="View Images ({{ $album->photos_count }})" href="{{route('albums.images',$album)}}" class="btn btn-primary">
+                                    <a title="View images" title="View Images ({{ $album->photos_count }})" href="{{route('albums.images',$album)}}" class="btn btn-primary">
                                         <span class="d-flex">
                                             <i class="bi bi-zoom-in"></i>
                                             ({{$album->photos_count}})
                                         </span>
                                     </a>
                                 @else
-                                    <a data-bs-toggle="tooltip" data-bs-title="No Images" disabled class="btn btn-primary pe-none"> 
+                                    <a title="No Images" disabled class="btn btn-primary pe-none"> 
                                         <i class="bi bi-zoom-in"></i>
                                     </a>
                                 @endif
@@ -63,7 +63,7 @@
                     <td>
                         <div class="row">
                             <div class="col-md-3 mx-2 mx-lg-0 col-lg-4">
-                                <a data-bs-toggle="tooltip" data-bs-title="Update Album" href="{{route('albums.edit',$album)}}" class="btn btn-primary"> <i
+                                <a title="Update Album" href="{{route('albums.edit',$album)}}" class="btn btn-primary"> <i
                                         class="bi bi-pen"></i></a>
                             </div>
                             <div class="col-md-3 mx-2 mx-lg-0 col-lg-4">
@@ -72,7 +72,7 @@
                                     @method('DELETE')
 
                                     @csrf
-                                    <button data-bs-toggle="tooltip" data-bs-title="Delete Album" class="btn btn-danger" id="{{$album->id}}"><i class="bi bi-trash"></i></button>
+                                    <button title="Delete Album" class="btn btn-danger" id="{{$album->id}}"><i class="bi bi-trash"></i></button>
                                 </form>
                             </div>
                         </div>
@@ -94,8 +94,8 @@
     @parent
     <script>
         $('document').ready(function () {
-            const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-            const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+            // const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+            // const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
             $('div.alert').fadeOut(5000);
             $('table').on('click', 'button.btn-danger', function (ele) {
                 ele.preventDefault();
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -27,18 +27,18 @@
                             <td>{{$cat->updated_at->diffForHumans()}}</td>
                             <td>
                                 @if ($cat->albums_count > 0)
-                                    <a class="btn btn-success" data-bs-toggle="tooltip" data-bs-title="Views Albums" class="" href="{{ route('albums.index') }}?category_id={{$cat->id}}">{{$cat->albums_count}}</a>
+                                    <a class="btn btn-success" title="Views Albums" class="" href="{{ route('albums.index') }}?category_id={{$cat->id}}">{{$cat->albums_count}}</a>
                                 @else
-                                    <a class="btn btn-default text-light" data-bs-toggle="tooltip" data-bs-title="No Albums" href="#">{{$cat->albums_count}}</a>
+                                    <a class="btn btn-default text-light" title="No Albums" href="#">{{$cat->albums_count}}</a>
                                 @endif
                             </td>
                             <td class="d-flex justify-content-between">
-                                <a id="upd-{{$cat->id}}" data-bs-toggle="tooltip" data-bs-title="Update Category" href="{{route('categories.edit',$cat->id)}}" class="btn btn-outline-info mx-1"> <i class="bi bi-pen"></i></a>
+                                <a id="upd-{{$cat->id}}" title="Update Category" href="{{route('categories.edit',$cat->id)}}" class="btn btn-outline-info mx-1"> <i class="bi bi-pen"></i></a>
                                 <form id="form{{$cat->id}}" method="POST" action="{{route('categories.destroy',$cat->id)}}"
                                     class="form-inline">
                                     @csrf
                                     @method('DELETE')
-                                    <button data-bs-toggle="tooltip" data-bs-title="Delete Category" class="mx-1 btn btn-outline-danger" id="btnDelete-{{ $cat->id }}"><i class="bi bi-trash"></i></button>
+                                    <button title="Delete Category" class="mx-1 btn btn-outline-danger" id="btnDelete-{{ $cat->id }}"><i class="bi bi-trash"></i></button>
                                 </form>
                             </td>
                         </tr>
@@ -80,8 +80,8 @@
     @parent
     <script>
         $('document').ready(function () {
-            const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-            const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+            // const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+            // const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
             $('div.alert').fadeOut(4000);
 
             //eliminazione categorie via AJAX
