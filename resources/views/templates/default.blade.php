f5c257067a58782f491ed1cd000618b912b22166
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-28 01:44:16 +0200

    delete header for add into the component

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -27,97 +27,19 @@
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
 </head>
 <body class="d-flex flex-column h-100">
+    
+    @include('components\header')
 
-<header>
-    <!-- Fixed navbar -->
-    <nav class="navbar border-bottom border-secondary border-bottom-1 navbar-expand-lg navbar-dark fixed-top bg-dark px-2">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('gallery.index') }}">GALLERY</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarCollapse">
-                @auth
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="{{ route('dashboard') }}">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('albums.index')}}">Albums</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('albums.create')}}">New Album</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('photos.create')}}">New Image</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{route('categories.index')}}">Categories</a>
-                        </li>
-                        @if(Auth::user()->user_role === 'admin')
-                            <li>
-                                <a class="nav-link" href="{{ route('users.index') }}">Admin</a>
-                            </li>
-                        @endif
-                    </ul>
-                @endauth
-                @guest
-                    <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <a class="nav-link" href="{{route('login')}}">Login</a>
-                        </li>
-                        <li>
-                            <a class="nav-link" href="{{route('register')}}">Register</a>
-                        </li>
-                    </ul>
-                @endguest
-                @auth
-                    <div class="dropdown">
-                        <div style="width:50px; height:50px;" class="d-inline-block">
-                            @if (Auth::user()->profile_pic) 
-                                <img style="width:50px; height:50px; object-fit:cover;" class="rounded-circle" src="{{ asset('storage/' .  Auth::user()->profile_pic) }}"/>
-                            @else
-                                <img style="width:50px; height:50px; object-fit:cover;" class="rounded-circle" src="{{asset('images/avatar.png')}}">   
-                            @endif
-                        </div>
-                        <a class="btn btn-default text-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
-                        data-bs-toggle="dropdown" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuLink">
-                            <li>
-                                <form id="logout-form" action="{{ route('logout')}}" method="POST" class="d-flex mb-0 justify-content-start align-items-center">
-                                    {{ csrf_field() }}
-                                    <button class="btn btn-default text-light">Logout</button>
-                                </form>
-                            </li>
-                            <li>
-                                <form class="d-flex mb-0 justify-content-start align-items-center" action="{{route('guestAdmin.destroy', Auth::user()->id)}}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-default text-light">Elimina Profilo</button>
-                                </form>
-                            </li>
-                        </ul>
-                    </div>
-                @endauth
-            </div>
-        </div>
-    </nav>
-</header>
-
-<main role="main" class="container-fluid mt-5 px-0">
-    @yield('content')
-    {{$slot ?? ''}}
-</main><!-- /.container -->
-@section('footer')
-    <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
-            crossorigin="anonymous"></script>
-    <script src="{{ url('/') }}/js/lightbox.js"></script>
-@show
+    <main role="main" class="container-fluid mt-5 px-0">
+        @yield('content')
+        {{$slot ?? ''}}
+    </main><!-- /.container -->
+    @section('footer')
+        <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+                integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+                crossorigin="anonymous"></script>
+        <script src="{{ url('/') }}/js/lightbox.js"></script>
+    @show
 </body>
 </html>
