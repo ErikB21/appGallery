18eeb4e7090754fe492adba42fe802c0873c94ec
ErikBorgogno <erikborgo.20@gmail.com>
2023-04-28 20:30:40 +0200

    change checkbox

--- resources/views/albums/partials/categoryCombo.blade.php
+++ resources/views/albums/partials/categoryCombo.blade.php
@@ -13,11 +13,24 @@
     <div class="form-check">
         <div class="row">
             @foreach($categories as $cat)
-                <div class="col-4">
-                    <input {{ in_array($cat->id, $selectedCategories, true)? 'checked' : ''  }} name="categories[]" type="checkbox" class="form-check-input" id="categories_{{$cat->id}}" value="{{$cat->id}}">
-                    <label class="form-check-label" for="categories">{{ Ucwords($cat->category_name) }}</label>
+                <div class="col-4 d-flex align-items-center">
+                    <input {{ in_array($cat->id, $selectedCategories, true)? 'checked' : ''  }} name="categories[]" type="checkbox" class="form-check-input circle_input" id="categories_{{$cat->id}}" value="{{$cat->id}}">
+                    <label class="form-check-label ps-2" for="categories">{{ Ucwords($cat->category_name) }}</label>
                 </div>
             @endforeach
         </div>
     </div>
 </div>
+
+<style>
+    .circle_input{
+        border-radius: 5px!important;
+        width: 20px!important;
+        height: 20px!important;
+        box-shadow: none!important;
+    }
+    .circle_input:checked{
+        background-color: #ff0057!important;
+        border: 0!important;
+    }
+</style>
