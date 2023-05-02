c8f0b191d3fb295dbdb73439e232dce96b03070a
ErikBorgogno <erikborgo.20@gmail.com>
2023-05-03 01:46:09 +0200

    add WebSite Name

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'EB-Gallery'),
 
     /*
     |--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://eb-appgallery-2101.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL'),
 
