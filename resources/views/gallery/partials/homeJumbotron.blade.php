eff25d1a1e102a5a4cadbf529b6a18325a815402
ErikBorgogno <erikborgo.20@gmail.com>
2023-05-03 17:38:19 +0200

    add bg-dark

--- resources/views/gallery/partials/homeJumbotron.blade.php
+++ resources/views/gallery/partials/homeJumbotron.blade.php
@@ -1,10 +1,12 @@
-<div class="d-flex title_all flex-column justify-content-center align-items-center">
-    <h1 class="py-5 text-center">AppGallery</h1>
-    <p class="text-light eb_p fw-bold text-center pb-4">“Fotografare è trattenere il respiro quando tutte le nostre facoltà convergono per captare la realtà fugace: a questo punto l'immagine diventa una grande gioia fisica ed intellettuale”</p>
+<div class="bg-dark">
+    <div class="d-flex title_all flex-column justify-content-center align-items-center">
+        <h1 class="py-5 text-center">AppGallery</h1>
+        <p class="text-light eb_p fw-bold text-center pb-4">“Fotografare è trattenere il respiro quando tutte le nostre facoltà convergono per captare la realtà fugace: a questo punto l'immagine diventa una grande gioia fisica ed intellettuale”</p>
+    </div>
+    <section class="container-fluid px-0">
+        <div class="parallax"></div>>
+    </section>
 </div>
-<section class="container-fluid px-0">
-    <div class="parallax"></div>>
-</section>
 <style>
     h1{
         font-size: 4rem;
