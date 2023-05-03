4e09785da759fc8eb6f59c59f048e5f9c49c91f3
ErikBorgogno <erikborgo.20@gmail.com>
2023-05-03 14:21:34 +0200

    Chnage GalleryController

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -2,24 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\{ Album, Category, Photo };
+use App\Models\{Album, Category, Photo};
+
 use Illuminate\Http\Request;
 
 class GalleryController extends Controller
 {
-    public function index(){
-        $albums = Album::with('categories')->latest()->paginate(12);
-        return view('gallery.albums')->with(['albums' => $albums,  'category_id' => null]);
+    public function index()
+    {
+        //  \DB::enableQueryLog();
+        $albums =  Album::with('categories')->latest()->paginate(50);
+
+        return view('gallery.albums')->with(['albums' => $albums,   'category_id' => null]);
     }
 
-    public function showAlbumImages(Album $album){
-        return view('gallery.images',['images' => Photo::whereAlbumId($album->id)->paginate(10), 'album' => $album ]);
+    public function showAlbumImages(Album $album)
+    {
+        return view('gallery.images',
+            [
+                'images' =>
+                Photo::whereAlbumId($album->id)->paginate(10),
+                'album' => $album
+            ]
+        );
     }
 
-    public function showCategoryAlbums(Category $category){
-        $cat = $category->albums()->with('categories')->latest()->paginate(50);
-        return view('gallery.albums')->with([
-            'albums' => $cat, 
-            'category_id' => $category->id]);
+    public function showCategoryAlbums(Category $category)
+    {
+
+        return view('gallery.albums')->with(
+            [
+                'category_id' => $category->id,
+                'albums' => $category->albums()->with('categories')->latest()->paginate(50)
+            ]
+
+        );
     }
 }
