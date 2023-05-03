478dd4faace55548bbe47be15359f5f41b900184
ErikBorgogno <erikborgo.20@gmail.com>
2023-05-03 02:48:08 +0200

    change GalleryController

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -8,17 +8,18 @@ use Illuminate\Http\Request;
 class GalleryController extends Controller
 {
     public function index(){
-        $albums = Album::with('categories')->latest()->paginate(9);
+        $albums = Album::with('categories')->latest()->paginate(12);
         return view('gallery.albums')->with(['albums' => $albums,  'category_id' => null]);
     }
 
     public function showAlbumImages(Album $album){
-        $photos = Photo::whereAlbumId($album->id)->paginate(9);
-        return view('gallery.images',['images' => $photos, 'album' => $album ]);
+        return view('gallery.images',['images' => Photo::whereAlbumId($album->id)->paginate(10), 'album' => $album ]);
     }
 
     public function showCategoryAlbums(Category $category){
-        $cat = $category->albums()->with('categories')->latest()->paginate();
-        return view('gallery.albums')->with(['albums' => $cat, 'category_id' => $category->id]);
+        $cat = $category->albums()->with('categories')->latest()->paginate(50);
+        return view('gallery.albums')->with([
+            'albums' => $cat, 
+            'category_id' => $category->id]);
     }
 }
