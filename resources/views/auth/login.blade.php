b3f44b5951ab8ebe7c38f5dc33c54c51c82c3886
ErikBorgogno <erikborgo.20@gmail.com>
2023-05-08 16:02:18 +0200

    change login component

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
     <section class="vh-100">
-        <div class="container-fluid h-custom mx-1" style="height: 90vh;">
+        <div class="container-fluid  h-custom" style="height: 90vh;">
             <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-md-9 col-lg-6 col-xl-5">
                 <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
